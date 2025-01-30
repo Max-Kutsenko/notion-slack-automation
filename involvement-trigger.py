8855f220310f5cfc80d28ad1ee011a5036736e39
maxonchickdev <timetotime828@gmail.com>
2025-01-30 12:48:22 +0200

    Add GitHub workflow to run Python script on schedule v5

--- involvement-trigger.py
+++ involvement-trigger.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'https://hooks.slack.com/services/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'
+url = 'https://hooks.slack.com/services/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'
 payload = {
     "text": "Hello, Slack!"
 }
