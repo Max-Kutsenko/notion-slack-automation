3803c272c2658542a10ac0c3b3918651e9ff3b2f
maxonchickdev <timetotime828@gmail.com>
2025-01-30 13:13:21 +0200

    Added correct workflow

--- involvement-trigger.py
+++ involvement-trigger.py
@@ -1,16 +1,12 @@
 import requests
 import json
 
-url = 'https://hooks.slack.com/services/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'
+url = 'https://hooks.slack.com/services/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'
 
 payload = {
-    "channel": "#trigger-from-notion-to-slack",
-    "username": "webhookbot",
-    "text": "This is posted to #trigger-from-notion-to-slack and comes from a bot named webhookbot.",
-    "icon_emoji": ":ghost:"
+    "text": "Hello, World!"
 }
 
 response = requests.post(url, json=payload)
 
-print(response.text)
 print(response.status_code)
